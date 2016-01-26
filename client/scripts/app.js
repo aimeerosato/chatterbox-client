9b4fdf7381f4b55aaee30f6364eb77d6109c8e0c
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-26 16:03:46 -0500

    addMessages, rooms almost done

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,61 +22,142 @@
 // Display messages recieved 
 
 $(document).ready(function() {
+  
+  var rooms = {};
+  var friends = {};
 
-  var app = {
-    init: function(){},
-    send: function(){},
-    fetch: function(){}
+
+  var app = {  
+    init: function(){
+        //event binding - submit button, click events, event listeners, run after everything
+      app.fetch(); 
+      roomHandler();
+    },
+    send: function(message){
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent. Data: ', data);
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message. Error: ', data);
+        }
+      });
+
+    },
+
+    fetch: function(){
+
+        $.ajax({
+          url: 'https://api.parse.com/1/classes/chatterbox',
+          type: 'GET',
+          //data: JSON.stringify(message),
+          contentType: 'application/json',
+
+          success:  function(data) {
+            dataHandler(data);
+            console.log('chatterbox: Message recieved. Data: ', data);
+            },
+
+          error: function (data) {
+            console.error('chatterbox: Failed to recieve message. Error: ', data);
+          }  
+
+           
+        });
+
+
+      }
+
+       
+
   };
 
-// POST Method - SEND METHOD
-
-var message = {
-  username: 'aimeeandfanny',
-  text: 'time zones different',
-  roomname: '4chan'
-};
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent. Data: ', data);
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message. Error: ', data);
-  }
-  });
-
-// GET Method - FETCH METHOD
-
-  $.ajax({
-        url: 'https://api.parse.com/1/classes/chatterbox',
-        type: 'GET',
-        //data: JSON.stringify(message),
-        contentType: 'application/json',
-        success:  function(data) {
-            console.log(GetMessages(data));
-            console.log('chatterbox: Message recieved. Data: ', data);
-          },
-        error: function (data) {
-          console.error('chatterbox: Failed to recieve message. Error: ', data);
-        }   
-  });
-
-  var GetMessages = function(dataFromServer){
-    for(var i = 0; i < dataFromServer.results.length; i++){
-      this.user = dataFromServer.results[i].username;
-      this.text = dataFromServer.results[i].text; 
-      $('.messageContainer').append('<div class="message">' + this.user + ':\n' + this.text + '\n' + '</div>');
+  var addRoom = function(rooms){
+    for(var key in rooms){
+      $('#roomSelect').append('<option value= "' + key + '">' + key + '</option>');
     }
   };
 
-  // DELETE Messages - Remove Method
+  var roomHandler = function(){
+    // $('#roomSelect').change(function(){
+    //   if($('#roomSelect').val() === data.results[i].roomname) {   
+    //       // empty container
+    //     $('.messageContainer').empty();
+    //     // append messages with roomname
+    //     addMessages(data.results[i])
+    //   }
+      
+    // })
+  }
+
+  var dataHandler = function (data){ 
+    for(var i = 0; i < data.results.length; i++){
+        addMessages(data.results[i]);
+    
+      // Check for rooms
+      if(!rooms[data.results[i].roomname]){
+        //console.log(data.results[i].roomname)
+        rooms[data.results[i].roomname] = data.results[i].roomname;
+      }
+      
+      // Check for friends
+      if(!friends[data.results[i].username]){
+        friends[data.results[i].username] = data.results[i].username;
+      }
+    }
+
+      $('#roomSelect').change(function(){
+        console.log($('#roomSelect').val());
+        $('.messageContainer').empty();
+        for(var i = 0; i < data.results.length; i++){
+
+          if(data.results[i].roomname !== undefined ){
+            if($('#roomSelect').val() === data.results[i].roomname) {   
+              // if(data.results[i] !== undefined && )
+              // empty container
+              
+              // append messages with roomname
+              addMessages(data.results[i]);
+            }
+          }
+        }
+    });
+    
+
+    addRoom(rooms);
+  };
+
+  var addMessages = function(message){
+    user = message.username;
+    text = message.text;  
+    $('.messageContainer').append('<div class="message">' + user + ':\n' + text + '\n' + '</div>');
+  };
+
+  app.init(); 
+  
+  //event handler
+
+    //clear container
+    //append with messages that only have that room
+  
+
+  var addFriend = function(){
+
+  };
+
+  var handleSubmit = function() {
+
+  };
+  
+
+
+
 
 });
 
