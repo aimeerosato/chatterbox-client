745a8831a20183b805338411d6ce54c27e5b32b9
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-25 22:52:46 -0500

    fetch started

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,59 +1,6 @@
-// YOUR CODE HERE:
-/*var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent. Data: ', data);
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message. Error: ', data);
-  }
-});*/
-
-// Display messages recieved 
-
-$(document).ready(function() {
-
-
-  $.ajax({
-        url: 'https://api.parse.com/1/classes/chatterbox',
-        type: 'GET',
-        //data: JSON.stringify(message),
-        contentType: 'application/json',
-        success:  function(data) {
-            console.log(getMessages(data));
-            console.log('chatterbox: Message recieved. Data: ', data);
-          },
-        error: function (data) {
-          console.error('chatterbox: Failed to recieve message. Error: ', data);
-        }   
-  });
-
-var getMessages = function(dataFromServer){
-  // [{sd: message}, {jr: fjdkfj}]
-  var messageData = [];
-  var userMessage = {};
-    messageData.user = dataFromServer.results[0].username;
-    messageData.text = dataFromServer.results[0].text; 
-    $('.messageContainer').append('<div class="message"></div>');
-    $('.messageContainer div').append(messageData.user + ':\n' + messageData.text + '\n');
-  }
-  return messageData;
-
-
-});
-
 // Setup refresh displayed messaged (automatically or with a button)
   // Escape user input
+  // Button on-click
 
 // Users select username
 
@@ -71,3 +18,65 @@ var getMessages = function(dataFromServer){
 // 'befriend' other uses by click on username
 
 // display messages by friends in bold
+
+// Display messages recieved 
+
+$(document).ready(function() {
+
+  var app = {
+    init: function(){},
+    send: function(){},
+    fetch: function(){}
+  };
+
+// POST Method - SEND METHOD
+
+var message = {
+  username: 'aimeeandfanny',
+  text: 'time zones different',
+  roomname: '4chan'
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent. Data: ', data);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message. Error: ', data);
+  }
+  });
+
+// GET Method - FETCH METHOD
+
+  $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'GET',
+        //data: JSON.stringify(message),
+        contentType: 'application/json',
+        success:  function(data) {
+            console.log(GetMessages(data));
+            console.log('chatterbox: Message recieved. Data: ', data);
+          },
+        error: function (data) {
+          console.error('chatterbox: Failed to recieve message. Error: ', data);
+        }   
+  });
+
+  var GetMessages = function(dataFromServer){
+    for(var i = 0; i < dataFromServer.results.length; i++){
+      this.user = dataFromServer.results[i].username;
+      this.text = dataFromServer.results[i].text; 
+      $('.messageContainer').append('<div class="message">' + this.user + ':\n' + this.text + '\n' + '</div>');
+    }
+  };
+
+  // DELETE Messages - Remove Method
+
+});
+
